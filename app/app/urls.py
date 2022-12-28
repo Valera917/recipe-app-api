bd1a594c0fee4e12ff6845027609a97e7aeed640
valera <valera@example.com>
2022-12-28 20:01:58 +0200

    add image api for recipe

--- app/app/urls.py
+++ app/app/urls.py
@@ -5,6 +5,8 @@ from drf_spectacular.views import (
     SpectacularRedocView,
     SpectacularSwaggerView
 )
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -18,3 +20,9 @@ urlpatterns = [
     path('api/user/', include('user.urls')),
     path('api/recipe/', include('recipe.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
